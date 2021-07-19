rezaies/moodle

MDL-71672 atto_managefiles: Focus the first file manager button

lib/editor/atto/plugins/managefiles/yui/build/moodle-atto_managefiles-button/moodle-atto_managefiles-button-debug.js
@@ -33,7 +33,9 @@ YUI.add('moodle-atto_managefiles-button', function (Y, NAME) {
  * @extends M.editor_atto.EditorPlugin
  */
 
-var LOGNAME = 'atto_managefiles';
+var LOGNAME = 'atto_managefiles',
+    CAN_RECEIVE_FOCUS_SELECTOR = '.fp-navbar a:not([disabled])',
+    FILE_MANAGER__SELECTOR = '#fitem_id_files_filemanager';
 
 Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], {
 
@@ -95,6 +97,22 @@ Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_at
             width: '100%'
         });
         iframe.setAttribute('src', this._getIframeURL());
+
+        // Focus on the first focusable element of the file manager after it is fully loaded.
+        iframe.on('load', function(e, frame) {
+            var fileManager = frame.getDOMNode().contentDocument.querySelector(FILE_MANAGER__SELECTOR);
+            // The file manager component is loaded asynchronously after the page is loaded.
+            // We check for the presence of .fm-loaded every 200 ms to determine if the file manager is loaded yet.
+            var intervalId = setInterval(function() {
+                if (fileManager.querySelector('.fm-loaded')) {
+                    var firstFocusableElement = fileManager.querySelector(CAN_RECEIVE_FOCUS_SELECTOR);
+                    if (firstFocusableElement) {
+                        firstFocusableElement.focus();
+                    }
+                    clearInterval(intervalId);
+                }
+            }, 200);
+        }, this, iframe);
 
         dialogue.set('bodyContent', iframe)
                 .show();

lib/editor/atto/plugins/managefiles/yui/build/moodle-atto_managefiles-button/moodle-atto_managefiles-button.js
@@ -33,7 +33,9 @@ YUI.add('moodle-atto_managefiles-button', function (Y, NAME) {
  * @extends M.editor_atto.EditorPlugin
  */
 
-var LOGNAME = 'atto_managefiles';
+var LOGNAME = 'atto_managefiles',
+    CAN_RECEIVE_FOCUS_SELECTOR = '.fp-navbar a:not([disabled])',
+    FILE_MANAGER__SELECTOR = '#fitem_id_files_filemanager';
 
 Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], {
 
@@ -93,6 +95,22 @@ Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_at
             width: '100%'
         });
         iframe.setAttribute('src', this._getIframeURL());
+
+        // Focus on the first focusable element of the file manager after it is fully loaded.
+        iframe.on('load', function(e, frame) {
+            var fileManager = frame.getDOMNode().contentDocument.querySelector(FILE_MANAGER__SELECTOR);
+            // The file manager component is loaded asynchronously after the page is loaded.
+            // We check for the presence of .fm-loaded every 200 ms to determine if the file manager is loaded yet.
+            var intervalId = setInterval(function() {
+                if (fileManager.querySelector('.fm-loaded')) {
+                    var firstFocusableElement = fileManager.querySelector(CAN_RECEIVE_FOCUS_SELECTOR);
+                    if (firstFocusableElement) {
+                        firstFocusableElement.focus();
+                    }
+                    clearInterval(intervalId);
+                }
+            }, 200);
+        }, this, iframe);
 
         dialogue.set('bodyContent', iframe)
                 .show();

lib/editor/atto/plugins/managefiles/yui/src/button/js/button.js
@@ -31,7 +31,9 @@
  * @extends M.editor_atto.EditorPlugin
  */
 
-var LOGNAME = 'atto_managefiles';
+var LOGNAME = 'atto_managefiles',
+    CAN_RECEIVE_FOCUS_SELECTOR = '.fp-navbar a:not([disabled])',
+    FILE_MANAGER__SELECTOR = '#fitem_id_files_filemanager';
 
 Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], {
 
@@ -93,6 +95,22 @@ Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_at
             width: '100%'
         });
         iframe.setAttribute('src', this._getIframeURL());
+
+        // Focus on the first focusable element of the file manager after it is fully loaded.
+        iframe.on('load', function(e, frame) {
+            var fileManager = frame.getDOMNode().contentDocument.querySelector(FILE_MANAGER__SELECTOR);
+            // The file manager component is loaded asynchronously after the page is loaded.
+            // We check for the presence of .fm-loaded every 200 ms to determine if the file manager is loaded yet.
+            var intervalId = setInterval(function() {
+                if (fileManager.querySelector('.fm-loaded')) {
+                    var firstFocusableElement = fileManager.querySelector(CAN_RECEIVE_FOCUS_SELECTOR);
+                    if (firstFocusableElement) {
+                        firstFocusableElement.focus();
+                    }
+                    clearInterval(intervalId);
+                }
+            }, 200);
+        }, this, iframe);
 
         dialogue.set('bodyContent', iframe)
                 .show();

verzog/moodle-editor_atto

plugins/managefiles/yui/build/moodle-atto_managefiles-button/moodle-atto_managefiles-button-debug.js
@@ -33,7 +33,9 @@ YUI.add('moodle-atto_managefiles-button', function (Y, NAME) {
  * @extends M.editor_atto.EditorPlugin
  */
 
-var LOGNAME = 'atto_managefiles';
+var LOGNAME = 'atto_managefiles',
+    CAN_RECEIVE_FOCUS_SELECTOR = '.fp-navbar a:not([disabled])',
+    FILE_MANAGER_SELECTOR = '#fitem_id_files_filemanager';
 
 Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], {
 
@@ -95,6 +97,22 @@ Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_at
             width: '100%'
         });
         iframe.setAttribute('src', this._getIframeURL());
+
+        // Focus on the first focusable element of the file manager after it is fully loaded.
+        iframe.on('load', function(e, frame) {
+            var fileManager = frame.getDOMNode().contentDocument.querySelector(FILE_MANAGER_SELECTOR);
+            // The file manager component is loaded asynchronously after the page is loaded.
+            // We check for the presence of .fm-loaded every 200 ms to determine if the file manager is loaded yet.
+            var intervalId = setInterval(function() {
+                if (fileManager.querySelector('.fm-loaded')) {
+                    var firstFocusableElement = fileManager.querySelector(CAN_RECEIVE_FOCUS_SELECTOR);
+                    if (firstFocusableElement) {
+                        firstFocusableElement.focus();
+                    }
+                    clearInterval(intervalId);
+                }
+            }, 200);
+        }, this, iframe);
 
         dialogue.set('bodyContent', iframe)
                 .show();

plugins/managefiles/yui/build/moodle-atto_managefiles-button/moodle-atto_managefiles-button.js
@@ -33,7 +33,9 @@ YUI.add('moodle-atto_managefiles-button', function (Y, NAME) {
  * @extends M.editor_atto.EditorPlugin
  */
 
-var LOGNAME = 'atto_managefiles';
+var LOGNAME = 'atto_managefiles',
+    CAN_RECEIVE_FOCUS_SELECTOR = '.fp-navbar a:not([disabled])',
+    FILE_MANAGER_SELECTOR = '#fitem_id_files_filemanager';
 
 Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], {
 
@@ -93,6 +95,22 @@ Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_at
             width: '100%'
         });
         iframe.setAttribute('src', this._getIframeURL());
+
+        // Focus on the first focusable element of the file manager after it is fully loaded.
+        iframe.on('load', function(e, frame) {
+            var fileManager = frame.getDOMNode().contentDocument.querySelector(FILE_MANAGER_SELECTOR);
+            // The file manager component is loaded asynchronously after the page is loaded.
+            // We check for the presence of .fm-loaded every 200 ms to determine if the file manager is loaded yet.
+            var intervalId = setInterval(function() {
+                if (fileManager.querySelector('.fm-loaded')) {
+                    var firstFocusableElement = fileManager.querySelector(CAN_RECEIVE_FOCUS_SELECTOR);
+                    if (firstFocusableElement) {
+                        firstFocusableElement.focus();
+                    }
+                    clearInterval(intervalId);
+                }
+            }, 200);
+        }, this, iframe);
 
         dialogue.set('bodyContent', iframe)
                 .show();

plugins/managefiles/yui/src/button/js/button.js
@@ -31,7 +31,9 @@
  * @extends M.editor_atto.EditorPlugin
  */
 
-var LOGNAME = 'atto_managefiles';
+var LOGNAME = 'atto_managefiles',
+    CAN_RECEIVE_FOCUS_SELECTOR = '.fp-navbar a:not([disabled])',
+    FILE_MANAGER_SELECTOR = '#fitem_id_files_filemanager';
 
 Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], {
 
@@ -93,6 +95,22 @@ Y.namespace('M.atto_managefiles').Button = Y.Base.create('button', Y.M.editor_at
             width: '100%'
         });
         iframe.setAttribute('src', this._getIframeURL());
+
+        // Focus on the first focusable element of the file manager after it is fully loaded.
+        iframe.on('load', function(e, frame) {
+            var fileManager = frame.getDOMNode().contentDocument.querySelector(FILE_MANAGER_SELECTOR);
+            // The file manager component is loaded asynchronously after the page is loaded.
+            // We check for the presence of .fm-loaded every 200 ms to determine if the file manager is loaded yet.
+            var intervalId = setInterval(function() {
+                if (fileManager.querySelector('.fm-loaded')) {
+                    var firstFocusableElement = fileManager.querySelector(CAN_RECEIVE_FOCUS_SELECTOR);
+                    if (firstFocusableElement) {
+                        firstFocusableElement.focus();
+                    }
+                    clearInterval(intervalId);
+                }
+            }, 200);
+        }, this, iframe);
 
         dialogue.set('bodyContent', iframe)
                 .show();